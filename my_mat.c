9e8c57bab1e60e0cc8139209b13b1a0c4b0053df
amitsteinmetz <amitsteinmetz@gmail.com>
2022-12-07 11:40:11 +0200

    add comments

--- my_mat.c
+++ my_mat.c
@@ -4,6 +4,7 @@
 
 void value_of_matrix(int input_matrix[MAT_SIZE][MAT_SIZE]) {
 
+// With this 'for' loops we can go to every component in the matrix and give it a value from user
     for (int i = 0; i < MAT_SIZE; ++i) {
         for (int j = 0; j < MAT_SIZE; ++j) {
             scanf(" %d", &input_matrix[i][j]);
@@ -13,6 +14,8 @@ void value_of_matrix(int input_matrix[MAT_SIZE][MAT_SIZE]) {
 
 void isPath(int input_matrix[MAT_SIZE][MAT_SIZE], int i, int j) {
     bool Is_Path = true;
+    // There is a condition in shortest_path func that depend in the boolean variant is_path:
+    // if true, the func will print only what rellevant to isPath func 
     shortest_path(input_matrix, i, j, Is_Path);
 
 }
@@ -23,13 +26,15 @@ void shortest_path(int input_matrix[MAT_SIZE][MAT_SIZE], int i, int j, bool is_p
 
     int solution_matrix[MAT_SIZE][MAT_SIZE] = {0};
 
-    // copy from input matrix to solution matrix:
+    // Copy from input matrix to solution matrix:
     for (int a=0; a<MAT_SIZE; a++) {
         for (int b = 0; b < MAT_SIZE; b++) {
             solution_matrix[a][b] = input_matrix[a][b];
         }
     }
-
+    
+    // Using Floyd–Warshall algorithm.
+    // In addition, we add conditions to prevent calculate non-exist edges
     for (int k = 0; k < MAT_SIZE; k++) {
         for (int l = 0; l < MAT_SIZE; l++) {
             for (int m = 0; m < MAT_SIZE; m++) {
@@ -45,6 +50,9 @@ void shortest_path(int input_matrix[MAT_SIZE][MAT_SIZE], int i, int j, bool is_p
             }
         }
     }
+    // Condition that depend in the boolean variant is_path (parameter that got from the caller):
+    // If true, the func will print only what rellevant to isPath func.
+    // If false, the func will print only what rellevant to shortest_path func.
     if (is_path) {
         printf("%s", (solution_matrix[i][j] != 0) ? "True\n" : "False\n");
     }
